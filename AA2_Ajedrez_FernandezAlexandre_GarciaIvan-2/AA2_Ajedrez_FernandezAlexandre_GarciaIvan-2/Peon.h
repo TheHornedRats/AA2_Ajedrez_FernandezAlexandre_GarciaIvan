51513235a80cd1ffb19358e6e3965e9848b65542
Alexandre Fernandez <alexandre.fernandez@enti.cat>
2025-01-24 19:04:53 +0100

    Cambios en el peon
    Cambios en el comportamiento del peon, ahora le he dado a la clase la capacidad de estudiar el tablero y asi puede reconocer que valor tienen las casillas, para asi detectar si puede mover en diagonal para comer o no.
    He implementado este movimiento en diagonal para el peon

--- AA2_Ajedrez_FernandezAlexandre_GarciaIvan-2/AA2_Ajedrez_FernandezAlexandre_GarciaIvan-2/Peon.h
+++ AA2_Ajedrez_FernandezAlexandre_GarciaIvan-2/AA2_Ajedrez_FernandezAlexandre_GarciaIvan-2/Peon.h
@@ -1,6 +1,7 @@
 #ifndef PEON_H
 #define PEON_H
 #include "Piezas.h"
+#include "config.h"
 
 
 class Peon : public PiezaMadre {
@@ -11,23 +12,37 @@ public:
 	}
 
 
-	bool MovimientoValido(int NuevaFila, int NuevaColumna) override {
+	bool MovimientoValido(int NuevaFila, int NuevaColumna, PiezaMadre* tablero[HEIGHT][WIDTH]) override {
 		//en esta logica se da por echo que los peones siempre avanzaran en linia recta, a menos que capturen pieza
 		//los peones blancos siempre iran hacia arriba con lo cual sus filas decreceran siempre y para los negros al reves
 		//si el peon se encuentra en su casilla inicial tambien se dara como valido un movimiento de dos casillas
 		if (columna == NuevaColumna){
 			if (color == 'B')
 			{
+				//aquesta part serveix perque en la posicio incial els peon puguin moure dues caselles
 				return (NuevaFila == fila - 1 || (fila == 6 && NuevaFila == fila - 2)) &&
-					(NuevaFila >= 0 && NuevaFila < 8);
+					tablero[NuevaFila][NuevaColumna] == nullptr; //amb això m'aseguro que només pot avançar endevant si la casella està buida
 			}
-		}
-		if (abs(NuevaColumna - columna) ==1 ) 
-		{
-			if (color == 'B') { return NuevaFila == fila - 1; }
 			else
 			{
-				return NuevaFila == fila + 1;
+				return (NuevaFila == fila + 1 || (fila == 2 && NuevaFila == fila + 2)) &&
+					tablero[NuevaFila][NuevaColumna] == nullptr; 
+			}
+		}
+
+		if (abs(NuevaColumna - columna) ==1 ) 
+		{
+			if (color == 'B') {
+				return NuevaFila == fila - 1 &&
+					tablero[NuevaFila][NuevaColumna] != nullptr &&
+					tablero[NuevaFila][NuevaColumna]->color != color;
+
+			}
+			else
+			{
+				return NuevaFila == fila + 1 &&
+					tablero[NuevaFila][NuevaColumna] != nullptr &&
+					tablero[NuevaFila][NuevaColumna]->color != color;
 
 			}
 

c87d3140310f1318f45fe5ea214ffdd73e33a836
Alexandre Fernandez <alexandre.fernandez@enti.cat>
2025-01-24 18:37:38 +0100

    Creo el config.h
    Creo el config.h para poner ahi las constantes y evitarnos magic numbers, es la segunda vez que lo creo porque he intentado subirlo al github y he perdido mucho progreso

---
+++ AA2_Ajedrez_FernandezAlexandre_GarciaIvan-2/AA2_Ajedrez_FernandezAlexandre_GarciaIvan-2/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Tamaño del tablero
+const int HEIGHT = 9; // 8 filas del tablero + 1 para rotulos
+const int WIDTH = 9;  // 8 columnas del tablero + 1 para rotulos
+
+// Representación de colores de las piezas
+const char COLOR_BLANCO = 'B'; // Blanco
+const char COLOR_NEGRO = 'N';  // Negro
+
+// Representación para casillas vacías
+const char CASILLA_VACIA = '.';
+
+// Rango de filas y columnas validas en el tablero
+const  int MIN_TABLERO = 1; // Primera fila y columna (despues de los rotulos)
+const  int MAX_TABLERO = 8; // Última fila y columna (tablero real)
+
+#endif // CONFIG_H
+
